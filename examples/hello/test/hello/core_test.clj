bdfb834a16d70c49b9e5feab8457ea820654fee9
Matt Rkiouak <mrkiouak@gmail.com>
2019-09-06 11:04:45 -0400

    Update example for 1.0.0-1.0.1 changes
    Signed-off-by: Matt Rkiouak <mrkiouak@gmail.com>

--- examples/hello/test/hello/core_test.clj
+++ examples/hello/test/hello/core_test.clj
@@ -3,17 +3,49 @@
             [clojure.pprint :refer [pprint]]
             [io.pedestal.test :refer :all]
             [io.pedestal.http :as bootstrap]
-            [hello.service :as service]))
+            [hello.service :as service]
+            [hello.test.utils :as test.utils]
+            [protojure.pedestal.core :as protojure.pedestal]
+            [protojure.internal.grpc.client.providers.http2.jetty :as jetty-client]
+            [protojure.grpc.client.providers.http2 :as grpc.http2]
+            [protojure.pedestal.routes :as pedestal.routes]
+            [clojure.core.async :as async :refer [go >! <!!]]
+            [com.example.addressbook :as example]
+            [com.example.addressbook.Greeter :as greeter]
+            [io.pedestal.http :as pedestal]
+            [io.pedestal.http.body-params :as body-params]
+            [protojure.grpc.client.api :as grpc]
+            [promesa.core :as p]))
 
-(def service
-  (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
+;;-----------------------------------------------------------------------------
+;; Fixtures
+;;-----------------------------------------------------------------------------
+(defonce test-env (atom {}))
 
-(deftest page-test
-  (is (.contains
-       (:body (response-for service :get "/about"))
-       "Clojure 1.9")))
+(defn create-service []
+      (let [port (test.utils/get-free-port)
+            interceptors [(body-params/body-params)
+                          pedestal/html-body]
+            server-params {:env                      :prod
+                           ::pedestal/routes         (into #{} service/grpc-routes)
+                           ::pedestal/port           port
+                           ::pedestal/type           protojure.pedestal/config
+                           ::pedestal/chain-provider protojure.pedestal/provider}]
 
-(deftest grpc-test
-  (is (.contains
-        (:body (response-for service :post "/com.example.addressbook.Greeter/Hello"))
-        "Hello, ")))
+           (let [server (test.utils/start-pedestal-server server-params)]
+                (swap! test-env assoc :port port :server server))))
+
+(defn destroy-service []
+      (swap! test-env update :server pedestal/stop))
+
+(defn wrap-service [test-fn]
+      (create-service)
+      (test-fn)
+      (destroy-service))
+
+(use-fixtures :once wrap-service)
+
+(deftest basic-grpc-check
+  (testing "Check that a round-trip GRPC request works"
+    (let [resp @(greeter/call-Hello @(grpc.http2/connect {:uri (str "http://localhost:" (:port @test-env))}) {:name "Jane Doe"})]
+      (is (= "Hello, Jane Doe" (:message resp))))))
