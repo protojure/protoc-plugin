ae0480f411f8b4cacdfa6716882673b41b76347a
Greg Haskins <greg@manetu.com>
2019-12-29 12:52:20 -0500

    Run 'lein ancient' on our example/hello project
    Purposely held back jetty http2-client to v9.x since v10.x is still -alpha
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- examples/hello/project.clj
+++ examples/hello/project.clj
@@ -5,24 +5,24 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2019
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.pedestal/pedestal.service "0.5.7"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
                  [protojure "1.0.1"]
                  ;; Include Undertow for supporting HTTP/2 for GRPCs
-                 [io.undertow/undertow-core "2.0.26.Final"]
-                 [io.undertow/undertow-servlet "2.0.26.Final"]
+                 [io.undertow/undertow-core "2.0.28.Final"]
+                 [io.undertow/undertow-servlet "2.0.28.Final"]
                  ;; And of course, protobufs
-                 [com.google.protobuf/protobuf-java "3.9.1"]
+                 [com.google.protobuf/protobuf-java "3.11.1"]
 
                  ;; -- PROTOC_GEN_CLOJURE CLIENT DEP --
-                 [org.eclipse.jetty.http2/http2-client "9.4.20.v20190813"]
+                 [org.eclipse.jetty.http2/http2-client "9.4.25.v20191220"]
 
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.28"]
-                 [org.slf4j/jcl-over-slf4j "1.7.28"]
-                 [org.slf4j/log4j-over-slf4j "1.7.28"]]
+                 [org.slf4j/jul-to-slf4j "1.7.30"]
+                 [org.slf4j/jcl-over-slf4j "1.7.30"]
+                 [org.slf4j/log4j-over-slf4j "1.7.30"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hello.server/run-dev"]}
