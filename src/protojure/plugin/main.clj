protojure/protoc-plugin

commit 30533eec4f764609862a375f5b1bcdc88ced071b

[#20] Add --version support
Signed-off-by: Greg Haskins <greg@manetu.com>

src/protojure/plugin/main.clj
@@ -3,7 +3,9 @@
 ;; SPDX-License-Identifier: Apache-2.0
 
 (ns protojure.plugin.main
-  (:require [protojure.protobuf :refer [->pb]]
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as string]
+            [protojure.protobuf :refer [->pb]]
             [com.google.protobuf :as protobuf]
             [com.google.protobuf.compiler :as compiler]
             [protojure.plugin.core :as core])
@@ -35,12 +37,50 @@
    (catch [:type :protoc-clj-abort :dummy :dummyval] {:keys [msg retval]}
      [retval msg])))
 
+(def options
+  [["-h" "--help"]
+   ["-v" "--version" "Print the version and exit"]])
+
+(defn exit [status msg & rest]
+  (do
+    (apply println msg rest)
+    status))
+
+(defn version [] (str "protoc-gen-clojure version: v" (System/getProperty "protoc-gen-clojure.version")))
+
+(defn prep-usage [msg] (->> msg flatten (string/join \newline)))
+
+(defn usage [options-summary]
+  (prep-usage [(version)
+               ""
+               "Usage: protoc-gen-clojure [options]"
+               ""
+               "Options:"
+               options-summary]))
+
+(defn -app
+  [& args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args options)]
+    (cond
+
+      (:help options)
+      (exit 0 (usage summary))
+
+      (not= errors nil)
+      (exit -1 "Error: " (string/join errors))
+
+      (:version options)
+      (exit 0 (version))
+
+      :else
+      (when-let [ret (execute (.. System in) (.. System out))]
+        (.println System/err (second ret))
+        (System/exit (first ret))))))
+
 (defn -main
   "plugin entrypoint: request/response protocol runs over stdin/out.
 
   protoc will send us a protobuf encoded CodeGeneratorRequest over stdin
   and expect a CodeGeneratorResponse on stdout."
   [& args]
-  (when-let [ret (execute (.. System in) (.. System out))]
-    (.println System/err (second ret))
-    (System/exit (first ret))))
+  (System/exit (apply -app args)))

project.clj
@@ -17,7 +17,8 @@
                  [org.antlr/ST4 "4.2"]
                  [slingshot "0.12.2"]
                  [camel-snake-kebab "0.4.1"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot protojure.plugin.main
   :bin {:name "protoc-gen-clojure"
         :bin-path "target"

test/protojure/plugin/core_test.clj
@@ -13,7 +13,8 @@
             [protojure.plugin.main :as main]
             [protojure.plugin.core :refer :all]
             [protojure.plugin.parse.core :refer :all]
-            [clojure.spec.test.alpha :as stest])
+            [clojure.spec.test.alpha :as stest]
+            [clojure.string :as string])
   (:import [java.io ByteArrayOutputStream]))
 
 (stest/instrument)
@@ -41,6 +42,12 @@
              '[com.example.kitchensink.Greeter.server :as example-server] :reload)))
 
 (generate-and-load-sample)
+
+(deftest cli-test
+  (testing "Ensure our CLI parsing works as advertised"
+    (is (= (main/-app "-h") 0))
+    (is (= (main/-app "-v") 0))
+    (is (= (main/-app "--foo") -1))))
 
 (deftest io-test
   (testing "Drive a sample request through plugin IO and ensure we get a 0 exit value"

commit d5c6164f1588b2d7fbb95109fd843c1396539935

[#23] Propagate exit status on success
Commit 30533eec introduced a bug where a successful compilation
would result in a crash in the plugin

Fixes #23

Signed-off-by: Greg Haskins <greg@manetu.com>

src/protojure/plugin/main.clj
@@ -73,9 +73,11 @@
       (exit 0 (version))
 
       :else
-      (when-let [ret (execute (.. System in) (.. System out))]
-        (.println System/err (second ret))
-        (System/exit (first ret))))))
+      (if-let [ret (execute (.. System in) (.. System out))]
+        (do
+          (.println System/err (second ret))
+          (first ret))
+        0))))
 
 (defn -main
   "plugin entrypoint: request/response protocol runs over stdin/out.