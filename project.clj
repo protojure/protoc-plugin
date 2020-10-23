57297592347070d9d13217454193266d72012e57
Greg Haskins <greg@manetu.com>
2020-10-23 08:50:42 -0400

    Run lein-ancient
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -12,14 +12,14 @@
             [lein-cloverage "1.0.11" :exclusions [org.clojure/clojure]]
             [lein-cljfmt "0.5.7" :exclusions [org.clojure/clojure]]]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [protojure "1.2.0"]
+                 [protojure "1.5.11"]
                  [protojure/google.protobuf "0.9.1"]
-                 [com.google.protobuf/protobuf-java "3.11.1"]
-                 [org.antlr/ST4 "4.2"]
+                 [com.google.protobuf/protobuf-java "3.13.0"]
+                 [org.antlr/ST4 "4.3.1"]
                  [slingshot "0.12.2"]
-                 [camel-snake-kebab "0.4.1"]
+                 [camel-snake-kebab "0.4.2"]
                  [org.clojure/math.combinatorics "0.1.6"]
-                 [org.clojure/tools.cli "0.4.2"]]
+                 [org.clojure/tools.cli "1.0.194"]]
   :main ^:skip-aot protojure.plugin.main
   :bin {:name "protoc-gen-clojure"
         :bin-path "target"
@@ -31,8 +31,8 @@
   ;; because that's where our development helper functions like (refresh) live.
   :repl-options {:init-ns user}
 
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.3.1"]
-                                  [io.pedestal/pedestal.service "0.5.7" :exclusions  [org.clojure/tools.reader]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.0.0"]
+                                  [io.pedestal/pedestal.service "0.5.8" :exclusions  [org.clojure/tools.reader]]
                                   [me.raynes/fs "1.4.6" :exclusions [org.apache.commons/commons-compress]]]
                    :source-paths ["target/test"]}
              :uberjar {:aot :all}})
