49f5259a46ef47b14fef4971fbba8bf0ecf75510
Greg Haskins <greg@manetu.com>
2021-12-27 14:39:08 -0500

    Update example/hello deps
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- examples/hello/project.clj
+++ examples/hello/project.clj
@@ -5,27 +5,27 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2019
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.pedestal/pedestal.service "0.5.7"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [protojure "1.2.0"]
+                 [protojure "1.7.0"]
                  ;; Include Undertow for supporting HTTP/2 for GRPCs
-                 [io.undertow/undertow-core "2.0.28.Final"]
-                 [io.undertow/undertow-servlet "2.0.28.Final"]
+                 [io.undertow/undertow-core "2.2.14.Final"]
+                 [io.undertow/undertow-servlet "2.2.14.Final"]
                  ;; And of course, protobufs
-                 [com.google.protobuf/protobuf-java "3.11.1"]
+                 [com.google.protobuf/protobuf-java "3.19.1"]
 
                  ;; -- PROTOC_GEN_CLOJURE CLIENT DEP --
-                 [org.eclipse.jetty.http2/http2-client "9.4.25.v20191220"]
+                 [org.eclipse.jetty.http2/http2-client "11.0.7"]
 
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.30"]
-                 [org.slf4j/jcl-over-slf4j "1.7.30"]
-                 [org.slf4j/log4j-over-slf4j "1.7.30"]]
+                 [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hello.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]]}
              :uberjar {:aot [hello.server]}}
   :main ^{:skip-aot true} hello.server)
--- examples/hello/src/com/example/addressbook/Greeter/client.cljc
+++ examples/hello/src/com/example/addressbook/Greeter/client.cljc
@@ -15,13 +15,15 @@
 ;-----------------------------------------------------------------------------
 
 (defn Hello
-  [client params]
+  ([client params] (Hello client {} params))
+  ([client metadata params]
   (let [input (async/chan 1)
         output (async/chan 1)
         desc {:service "com.example.addressbook.Greeter"
               :method  "Hello"
               :input   {:f com.example.addressbook/new-Person :ch input}
-              :output  {:f com.example.addressbook/pb->HelloResponse :ch output}}]
+              :output  {:f com.example.addressbook/pb->HelloResponse :ch output}
+              :metadata metadata}]
     (-> (send-unary-params input params)
-        (p/then (fn [_] (invoke-unary client desc output))))))
+        (p/then (fn [_] (invoke-unary client desc output)))))))
 
