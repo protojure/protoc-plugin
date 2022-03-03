e56da4c97af7f1139945c8cb84543550972ac1db
Greg Haskins <greg@manetu.com>
2022-03-03 17:16:20 -0500

    Release v2.1.0
    N.B. This release includes a fix for #58, which may break clients that relied
    upon the broken behavior.
    
    Changes since v2.0.0
    ---------------------
    d032809 Run lein-ancient
    2bfae57 Bump protojure library versions to 2.0.9
    624b803 Add self to CONTRIBUTORS.md
    ce41b57 Generate correct RPC service names in presence of java_package option
    7f870fa Test case for issue #58: java_package generates wrong service name
    acba0e2 Define service name in generated gRPC namespaces
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protoc-gen-clojure "2.0.1-SNAPSHOT"
+(defproject protoc-gen-clojure "2.1.0"
   :description "Protobuf protoc compiler plugin to generate native Clojure support for Google Protocol Buffers and GRPC"
   :url "http://github.com/protojure/protoc-plugin"
   :license {:name "Apache License 2.0"
