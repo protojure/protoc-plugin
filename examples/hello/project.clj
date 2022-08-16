1b650cbf4724d33c8e1720d296191255ec78e75c
Greg Haskins <greg@manetu.com>
2022-08-16 09:18:30 -0400

    Update deps
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- examples/hello/project.clj
+++ examples/hello/project.clj
@@ -5,20 +5,20 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"
             :year 2019
             :key "apache-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [io.pedestal/pedestal.service "0.5.9"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [io.pedestal/pedestal.service "0.5.10"]
 
                  ;; -- PROTOC-GEN-CLOJURE --
-                 [io.github.protojure/grpc-server "2.0.1"]
+                 [io.github.protojure/grpc-server "2.3.1"]
 
-                 [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.32"]
-                 [org.slf4j/jcl-over-slf4j "1.7.32"]
-                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
+                 [ch.qos.logback/logback-classic "1.2.11" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.36"]
+                 [org.slf4j/jcl-over-slf4j "1.7.36"]
+                 [org.slf4j/log4j-over-slf4j "1.7.36"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hello.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]
-                                  [io.github.protojure/grpc-client "2.0.1"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.10"]
+                                  [io.github.protojure/grpc-client "2.3.1"]]}
              :uberjar {:aot [hello.server]}}
   :main ^{:skip-aot true} hello.server)
